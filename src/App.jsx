212ac1eef285f9b46af3ddc3752fa0fa071c874c
Vasuki Rohilla <vasuki.rohilla@thoughtworks.com>
2023-12-19 13:12:45 +0530

    Adding dynamic component: Images here

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+import reactImage from "./assets/react-core-concepts.png"
 const reactDescrition = ['Fundamental', 'Crucial', 'Core'] 
 const randomIndex = (max)=>{
 return Math.floor( Math.random()*(max));
@@ -9,7 +9,7 @@ const Header = () => {
   const description = reactDescrition[randomIndex(3)]
   return (
       <header>
-        <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+        <img src={reactImage} alt="Stylized atom" />
         <h1>React Essentials</h1>
         <p>
           {description} React concepts you will need for almost any app you are
