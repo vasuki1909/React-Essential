7e84e79d830023035147ec1ff4a76aa558239876
Vasuki Rohilla <vasuki.rohilla@thoughtworks.com>
2023-12-22 18:59:44 +0530

    Learning props: Add. another way of sending props using object destructing

--- src/App.jsx
+++ src/App.jsx
@@ -24,23 +24,24 @@ const Header = () => {
 };
 
 function App() {
-  const list = CORE_CONCEPTS_LIST.map((data) => {
-   
-   return <CoreConcepts
-      img={data.image}
-      title={data.title}
-      description={data.description}
-    />;
-    console.log(data); 
-  });
-  console.log(list);
   return (
     <div>
       <Header />
       <main>
         <section id="core-concepts">
           <h2>Core Concepts</h2>
-          <ul>{list}</ul>
+          <ul>
+            <CoreConcepts
+              img={CORE_CONCEPTS_LIST[0].img}
+              title={CORE_CONCEPTS_LIST[0].title}
+              description={CORE_CONCEPTS_LIST[0].description}
+            />
+
+            <CoreConcepts {...CORE_CONCEPTS_LIST[1]} />
+            <CoreConcepts {...CORE_CONCEPTS_LIST[2]} />
+            <CoreConcepts {...CORE_CONCEPTS_LIST[3]} />
+
+          </ul>
         </section>
       </main>
     </div>
