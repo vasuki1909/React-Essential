42dad439d2f93c91549e7c920bf5390feb6b153d
Vasuki Rohilla <vasuki.rohilla@thoughtworks.com>
2023-12-23 15:07:37 +0530

    Spilling custom components into respective jsx files in component folder.

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,8 @@
 import React from "react";
-import reactImage from "./assets/react-core-concepts.png";
 import CoreConcepts from "./components/CoreConcepts";
+import Header from "./components/Header";
 import { CORE_CONCEPTS_LIST } from "./data";
 
-const reactDescrition = ["Fundamental", "Crucial", "Core"];
-const randomIndex = (max) => {
-  return Math.floor(Math.random() * max);
-};
-
-const Header = () => {
-  const description = reactDescrition[randomIndex(3)];
-  console.log(description);
-  return (
-    <header>
-      <img src={reactImage} alt="Stylized atom" />
-      <h1>React Essentials</h1>
-      <p>
-        {description} React concepts you will need for almost any app you are
-        going to build!
-      </p>
-    </header>
-  );
-};
 
 function App() {
   return (
---
+++ src/components/Header.jsx
@@ -0,0 +1,22 @@
+import reactImage from "../assets/react-core-concepts.png";
+
+const reactDescrition = ["Fundamental", "Crucial", "Core"];
+const randomIndex = (max) => {
+  return Math.floor(Math.random() * max);
+};
+const Header = () => {
+  const description = reactDescrition[randomIndex(3)];
+  console.log(description);
+  return (
+    <header>
+      <img src={reactImage} alt="Stylized atom" />
+      <h1>React Essentials</h1>
+      <p>
+        {description} React concepts you will need for almost any app you are
+        going to build!
+      </p>
+    </header>
+  );
+};
+
+export default Header
